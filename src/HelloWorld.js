ff33ba6d1403c6250dcd6e05ad3fdc4d7c3765e2
Paahn <paahndev@gmail.com>
2021-04-24 16:29:35 -0700

    commented out

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -1,32 +1,32 @@
-import { gql, useQuery } from '@apollo/client';
+// import { gql, useQuery } from '@apollo/client';
 
-const GET_STEPZEN = gql`
-  query MyHelloWorldQuery {
-    helloWorld {
-      message
-    }
-  }
-`;
+// const GET_STEPZEN = gql`
+//   query MyHelloWorldQuery {
+//     helloWorld {
+//       message
+//     }
+//   }
+// `;
 
-function HelloWorld() {
-  const { loading, error, data } = useQuery(GET_STEPZEN);
+// const HelloWorld = () => {
+//   const { loading, error, data } = useQuery(GET_STEPZEN);
 
-  if (error) return <p>{JSON.stringify(error)}</p>;
-  if (loading) return <p>Loading ...</p>;
+//   if (error) return <p>{JSON.stringify(error)}</p>;
+//   if (loading) return <p>Loading ...</p>;
 
-  return (
-    <>
-      <p>StepZen says: "{data.helloWorld.message}"</p>
-      <a
-        className="App-link"
-        href="https://stepzen.com"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn StepZen
-      </a>
-    </>
-  );
-}
+//   return (
+//     <>
+//       <p>StepZen says: "{data.helloWorld.message}"</p>
+//       <a
+//         className="App-link"
+//         href="https://stepzen.com"
+//         target="_blank"
+//         rel="noopener noreferrer"
+//       >
+//         Learn StepZen
+//       </a>
+//     </>
+//   );
+// }
 
-export default HelloWorld;
+// export default HelloWorld;
